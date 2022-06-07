3b56216961d8c2d008ec4081dd295f87545671df
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-06-07 15:04:38 +0300

    added categories

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className='category-container'>
+         <img src="#" alt="" />
+         <h2>Hats</h2>
+         <p>Shop Now</p>   
+      </div> 
+      <div className='category-container'>
+         <img src="#" alt="" />
+         <h2>Jackets</h2>
+         <p>Shop Now</p>   
+      </div> 
+      <div className='category-container'>
+         <img src="#" alt="" />
+         <h2>Sneakers</h2>
+         <p>Shop Now</p>   
+      </div> 
+      <div className='category-container'>
+         <img src="#" alt="" />
+         <h2>Womens</h2>
+         <p>Shop Now</p>   
+      </div> 
+      <div className='category-container'>
+         <img src="#" alt="" />
+         <h2>Mens</h2>
+         <p>Shop Now</p>   
+      </div> 
+    </div>
+  );
+}
+
+export default App;
