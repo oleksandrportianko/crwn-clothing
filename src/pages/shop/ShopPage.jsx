9274ba02922e2c81468baeeb9c5c14ce633cc454
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-06-16 19:34:58 +0300

    styling a shop page and added product card component

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,17 +1,16 @@
 import React, { useContext } from 'react'
-import './ShopPage.scss'
+import ProductCard from '../../components/product-card/ProductCard'
 import { ProductsContext } from '../../contexts/ProductsContext'
+import './ShopPage.scss'
 
 const ShopPage = () => {
    const { products } = useContext(ProductsContext)
 
    return (
-      <div>
+      <div className='products-container'>
          { 
-            products && products.map(product => (
-               <div key={product.id}>
-                  <h1>{product.name}</h1>
-               </div>
+            products && products.map(products => (
+               <ProductCard key={products.id} products={products} />
             ))
          }
       </div>
