a36e5b7d50874222bd8fe991e5b2369464d39f70
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-06-29 18:27:02 +0300

    added reducer for user context

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,15 +1,46 @@
-import { createContext, useState, useEffect } from 'react'
+import { type } from '@testing-library/user-event/dist/type'
+import { createContext, useEffect, useReducer } from 'react'
 import { onAuthStateChangedListener, createDocumentUserFromAuth } from '../utils/firebase/firebase'
+
+const SET_USER_DATA = 'SET-USER-DATA';
 
 export const UserContext = createContext({
    currentUser: null,
    setCurrentUser: () => null,
 }) 
 
-export const UserProdiver = ({ children }) => {
-   const [currentUser, setCurrentUser] = useState(null)
-   const value = { currentUser, setCurrentUser }
+const userReducer = (state, action) => {
+   const { type, payload } = action;
+   console.log('in user reducer')
+   
+   switch(type) {
+      case SET_USER_DATA:
+         return {
+            ...state,
+            currentUser: payload
+         }
+      default:
+         return {
+            ...state
+         }
+   }
+}
 
+const INITIAL_STATE = {
+   currentUser: null,
+}
+
+export const UserProdiver = ({ children }) => {
+   // const [currentUser, setCurrentUser] = useState(null)
+   const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE)
+   console.log(currentUser)
+
+   const setCurrentUser = (user) => {
+      dispatch({ type: SET_USER_DATA, payload: user })
+   }
+   
+   const value = { currentUser, setCurrentUser }
+   
    useEffect(() => {
       onAuthStateChangedListener((user) => {
          if (user) {
