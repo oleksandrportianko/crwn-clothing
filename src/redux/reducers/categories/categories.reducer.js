92c52919e05bec3e11277e1b9e75d13790f83116
Oleksandr <portianko123@gmail.com>
2022-08-25 12:14:07 +0300

    added action for thunks in categories reducer

--- src/redux/reducers/categories/categories.reducer.js
+++ src/redux/reducers/categories/categories.reducer.js
@@ -2,16 +2,30 @@ import { CATEGORIES_ACTION_TYPES } from './categories.types'
 
 const INITIAL_STATE = {
    categoriesArray: [],
+   isLoading: false,
+   error: null,
 }
 
 export const categoriesReducer = (state = INITIAL_STATE, action) => {
    const { type, payload } = action;
 
    switch(type) {
-      case CATEGORIES_ACTION_TYPES.SET_ALL_CATEGORIES:
+      case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
          return {
             ...state,
-            categoriesArray: payload
+            isLoading: true,
+         }
+      case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_ERROR:
+         return {
+            ...state,
+            isLoading: false,
+            error: payload,
+         }
+      case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+         return {
+            ...state,
+            categoriesArray: payload,
+            isLoading: false,
          }
       default:
          return state
