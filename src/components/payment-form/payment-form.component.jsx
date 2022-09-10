4bde490626ffaaeaae34b2fb80a3e5194db5967d
Oleksandr <portianko123@gmail.com>
2022-09-10 17:40:40 +0300

    update stripe methods

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -16,11 +16,40 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                amount: '10000'
+            })
+        }).then(res => res.json())
+
+        const clientSecret = response.paymentIntent.client_secret
+
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Oleksandr Portianko'
+                }
+            }
+        })
+
+        if (paymentResult.error) {
+            alert(paymentResult.error)
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment successful')
+            }
+        }
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit card payment:</h2>
                 <CardElement />
                 <Button buttonType={buttonVariables.inverted}>Pay now</Button>
