df093c405bad194a3aeca46991b3149d01d64baa
Oleksandr <portianko123@gmail.com>
2022-10-11 19:07:17 +0300

    creted observable withMatch function type

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,23 @@ export type Matchable<AC extends () => AnyAction> = AC & {
     match(action: AnyAction): action is ReturnType<AC>
 }
 
+export function withMatch<AC extends () => AnyAction & { type: string }>(actionCreator: AC): Matchable<AC>;
+
+export function withMatch<AC extends (...args: any[]) => AnyAction & { type: string }>(actionCreator: AC): Matchable<AC>
+
+export function withMatch(actionCreator: Function) {
+    const type = actionCreator().type
+    return Object.assign(
+        actionCreator,
+        {
+            type,
+            match(action: AnyAction) {
+                return action.type === type
+            }
+        }
+    )
+}
+
 export type Category = {
     title: string,
     imageUrl: string,
